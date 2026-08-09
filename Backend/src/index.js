a9a090739d10c7fc64ddac155df95488fcea423e
Aman ojha <amanojha6969@gmail.com>
2026-08-09 12:22:35 +0530

    Fix production frontend path

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -31,11 +31,21 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+//   app.get("/{*splat}", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+//   });
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "..", "Frontend", "dist");
+
+  app.use(express.static(frontendPath));
 
   app.get("/{*splat}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
